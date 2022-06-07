7c2a6233b65230af0fbd76a34422688e0efa2133
DESKTOP-EQSEK8E\Guest1 <duchiang93@gmail.com>
2022-06-07 16:31:37 +0800

    0607 my-cv

---
+++ src/components/Footer.js
@@ -0,0 +1,32 @@
+import React, { Fragment } from "react";
+
+export const Footer = () => {
+  return (
+    <div class="container">
+      <div>
+        <h1 class="title">Contact</h1>
+        <div class="footer-content">
+          <ul class="content_link">
+            <li>
+              <h4>Sharon's Web</h4>
+            </li>
+            <li>
+              <a href="#"></a>
+              <a href="#"></a>
+              <a href="#"></a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <ul class="content_info">
+            <li>地址</li>
+            <li>電話</li>
+            <li>信箱</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
