0cfff4c7fd239eacdb686ea5ac0e16a6966f5f5c
DESKTOP-EQSEK8E\Guest1 <duchiang93@gmail.com>
2022-06-13 16:10:04 +0800

    0613

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,43 +3,44 @@ import Icons from "./Icons";
 
 const Nav = () => {
   return (
-    <nav>
-      <div className="navigation">
-        <ul>
-          <li class="list">
-            <a href="#">
-              <span className="icon">
-                <Icons.AboutMe />
-              </span>
-              <span className="text">About Me</span>
-            </a>
-          </li>
-          <li class="list">
-            <a href="#">
-              <span className="icon">
-                <Icons.Skill width={50} />
-              </span>
-              <span className="text">Skill</span>
-            </a>
-          </li>
-          <li class="list">
-            <a href="#">
-              <span className="icon">
-                <Icons.Project />
-              </span>
-              <span className="text">Project</span>
-            </a>
-          </li>
-          <li class="list">
-            <a href="#">
-              <span className="icon">
-                <Icons.Contact />
-              </span>
-              <span className="text">Contact</span>
-            </a>
-          </li>
-        </ul>
+    <nav className="nav_container">
+      <div className="logo">
+        <h1>LOGO</h1>
       </div>
+      <ul className="nav_grid">
+        <li class="list">
+          <a href="#">
+            <span>
+              <Icons.AboutMe className="icon" />
+            </span>
+            <span className="text">About Me</span>
+          </a>
+        </li>
+        <li class="list">
+          <a href="#">
+            <span>
+              <Icons.Skill className="icon" />
+            </span>
+            <span className="text">Skill</span>
+          </a>
+        </li>
+        <li class="list">
+          <a href="#">
+            <span>
+              <Icons.Project className="icon" />
+            </span>
+            <span className="text">Project</span>
+          </a>
+        </li>
+        <li class="list">
+          <a href="#">
+            <span>
+              <Icons.Contact className="icon" />
+            </span>
+            <span className="text">Contact</span>
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 };
