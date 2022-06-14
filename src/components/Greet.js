994c8ea307a6ec416b3af11337a4e7eb7dfaf2b8
Sharon <duchiang93@gmail.com>
2022-06-14 22:23:12 +0800

    06/14Greet

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -2,9 +2,15 @@ import React from "react";
 
 const Greet = () => {
   return (
-    <div className="colourUnder">
-      <div className="text">Sharon</div>
-      <img src="/IMG/My pic.jpg" alt="" />
+    <div className="greet_container">
+      <div className="greet_top">
+        <p>Sharon's</p>
+        <p>portfolio</p>
+        <h3>Front-end Engineer</h3>
+      </div>
+      <div className="greet_bottom">
+        <img src="/IMG/My pic.jpg" alt="" />
+      </div>
     </div>
   );
 };
