18776d55e8a3993bf8570f8a8e632209538789f5
DESKTOP-EQSEK8E\Guest1 <duchiang93@gmail.com>
2022-06-22 16:26:08 +0800

    0622

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -7,12 +7,15 @@ const Project = () => {
       <ul className="row tab_content">
         <div className="project_grid">
           <li className="work_card ">
-            <a href="https://witega.com.tw/" target="_blank">
+            <a
+              href=" https://duchiang93.github.io/photo-website/"
+              target="_blank"
+            >
               <div className="work_list_img">
-                <img src="/IMG/witega.jpg" alt="" />
+                <img src="/IMG/photo-website.jpg" alt="" />
               </div>
             </a>
-            <h4>Witega 威特嘉科技開發股份有限公司</h4>
+            <h4>Photo Website</h4>
           </li>
           <li className="work_card ">
             <a href="https://witega.com.tw/" target="_blank">
